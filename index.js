31a2aa6a90b0c0b829464396959c0b2e88c852ac
briitogabriel <gabriel.brito.a@hotmail.com>
2023-04-12 21:21:49 -0300

    findPatient route + query params

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ console.log('Connected to database.');
 const createPatient = require('./src/controllers/patient/createPatient');
 const updatePatient = require('./src/controllers/patient/updatePatient');
 const updateServiceStatus = require('./src/controllers/patient/updateServiceStatus');
+const findPatient = require('./src/controllers/patient/findPatient');
 
 app.use(express.json());
 
 app.post('/api/patients', createPatient)
 app.put('/api/patients/:id', updatePatient)
 app.patch('/api/patients/:id/status', updateServiceStatus)
+app.get('/api/patients', findPatient)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {console.log(`App listenig on PORT ${PORT}`)});
---
+++ src/controllers/patient/findPatient.js
@@ -0,0 +1,35 @@
+const Patient = require('../../models/patient');
+
+async function findPatient (req, res) {
+
+  try {
+    const statusParam = req.query.status
+    const statusList = ['AGUARDANDO_ATENDIMENTO', 'EM_ATENDIMENTO', 'ATENDIDO', 'NAO_ATENDIDO'];
+    const databaseStatus = ['AGUARDANDO ATENDIMENTO', 'EM ATENDIMENTO', 'ATENDIDO', 'NÃO ATENDIDO'];
+
+    if (statusParam) {
+      if (!statusList.includes(statusParam.toUpperCase())) {
+        return res.status(400).json({message: "Informe um status válido ('AGUARDANDO_ATENDIMENTO', 'EM_ATENDIMENTO', 'ATENDIDO', 'NAO_ATENDIDO') ou remova o campo para listar todos os status."})
+      }
+      const indexPosition = statusList.indexOf(statusParam.toUpperCase())
+      const statusToFilter = databaseStatus[indexPosition]
+
+      const findFilteredPatients = await Patient.findAll({ where:
+        { service_status: statusToFilter }
+      });
+      if (findFilteredPatients.length == 0) {
+        return res.status(404).json({message: `Nenhum paciente com status '${statusToFilter}' encontrado.`})
+      }
+      return res.status(200).json(findFilteredPatients);
+    }
+
+    const allPatients = await Patient.findAll();
+    res.status(200).json(allPatients);
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({error: 'Não foi possível processar sua solicitação.'});
+  }
+}
+
+module.exports = findPatient;
